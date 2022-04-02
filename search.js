c7a8b295ebd3aa71afa1e81d5d42e8dc7efed626
LollyJollyCoconut <danielpeng929@gmail.com>
2022-04-01 20:33:08 -0700

    e
    e

--- search.js
+++ search.js
@@ -2,6 +2,18 @@ let searchButton = document.querySelector(".search-ingredients-button");
 let searchText = document.querySelector(".search-ingredients-text");
 let apiURL;
 let apiResponse;
+let maxReadyTimeSlider = document.querySelector("#max-ready-time-slider");
+let maxReadyTimeValue = null;
+let maxReadyTimeSpan = document.querySelector(".included-filters-max-ready-time");
+let maxReadyTimeSpan2 = document.querySelector(".filter-ready-time-value");
+let maxCaloriesValue = null;
+let maxCaloriesSlider = document.querySelector("#max-calories-slider");
+let maxCaloriesSpan = document.querySelector(".included-filters-max-calories");
+let maxCaloriesSpan2 = document.querySelector(".filter-max-calories-value");
+let dietsList = ["gluten free", "ketogenic", "vegetarian", "lacto vegetarian", "ovo vegetarian", "vegan", "pescetarian", "paleo", "primal", "low fodmap", "whole30"];
+let dietSpan = document.querySelector(".included-filters-diet");
+let dietForm = document.querySelector("#collapse-filter-diet");
+let dietValue = null;
 let includedIngredientsList = [];
 let excludedIngredientsList = [];
 let includedIngredientsSpan = document.querySelector(".included-ingredients");
@@ -9,7 +21,7 @@ let excludedIngredientsSpan = document.querySelector(".excluded-ingredients");
 let ingredientsSearchSection = document.getElementById("ingredients-search-section");
 categoriesList.forEach(function(category) {
 	let categoryClass = category.replaceAll(' ','-');
-	ingredientsSearchSection.innerHTML += `<section class = "ingredients-category-list ${categoryClass} col-md-6">
+	ingredientsSearchSection.innerHTML += `<section class = "ingredients-category-list ${categoryClass} col-md-6 px-2">
 		<div class = "ingredients-category-header">
 			<button class = "btn btn-outline-dark show-ingredients-button col-12" type = "button" data-bs-toggle = "collapse" data-bs-target = "#collapse-${categoryClass}" aria-expanded = "false" aria-controls = "collapse-${categoryClass}">
 				<span class = "ingredients-category-heading">${category} </span>
@@ -73,10 +85,45 @@ searchForm.addEventListener('submit', function (event) {
 	event.preventDeafault();
 	event.stopPropagation();
 }, false);
+maxReadyTimeSlider.addEventListener("change",function(event) {
+	maxReadyTimeValue = maxReadyTimeSlider.value;
+	maxReadyTimeSpan.innerText = `Max Ready Time: ${maxReadyTimeValue} minutes`;
+	maxReadyTimeSpan2.innerText = `${maxReadyTimeValue} minutes`;
+});
+maxCaloriesSlider.addEventListener("change", function(event) {
+	maxCaloriesValue = maxCaloriesSlider.value;
+	maxCaloriesSpan.innerText = `Max Calories: ${maxCaloriesValue} calories`;
+	maxCaloriesSpan2.innerText = `${maxCaloriesValue} calories`;
+});
+dietsList.forEach(function(diet) {
+
+	let dietClass = diet.replaceAll(` `,`-`);
+	dietForm.innerHTML += `<div class = "form-check">
+		<input class = "form-check-input"type = "radio" name = "filter-diet" id = "${dietClass}">
+		<label class = "form-check-label" for = "${dietClass}">
+			${diet}
+		</label>
+	</div>`;
+});
+dietForm.addEventListener("click", function(event) {
+	if (event.target.getAttribute(`name`) == `filter-diet`) {
+		dietSpan.innerText = `Diet: ${event.target.id}`;
+		dietValue = event.target.id.replaceAll(`-`,`%20`);
+	}
+});
 searchButton.addEventListener("click", function(event) {
-	apiURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apikey}&addRecipeInformation=true`;
+	apiURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apikey}&addRecipeInformation=true&number=100`;
 	if (searchText.value != "") {
 		apiURL += `&query=${searchText.value}`;
+	}
+	if (maxReadyTimeValue !=null) {
+		apiURL += `&maxReadyTime=${maxReadyTimeValue}`;
+	}
+	if (maxCaloriesValue !=null) {
+		apiURL += `&maxCalories=${maxCaloriesValue}`;
+	}
+	if (dietValue != null) {
+		apiURL += `&diet=${dietValue}`;
 	}
 	fetch(apiURL, {
 		"method": "GET"
