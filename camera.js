97074dd79ea3613d05ea78362ae18053e4fdb710
LollyJollyCoconut <danielpeng929@gmail.com>
2023-08-28 06:02:55 -0700

    e
    e

--- camera.js
+++ camera.js
@@ -1,6 +1,7 @@
 let video = document.getElementById("video");
 let message = document.getElementById("message");
 let logo = document.getElementById("camera-logo");
+let takePhotoButton = document.querySelector(".button-one");
 let cameraThing = document.getElementById("camera-parent-div");
 if (navigator.mediaDevices.getUserMedia) {
 	navigator.mediaDevices.getUserMedia({ video: true })
@@ -16,3 +17,61 @@ if (navigator.mediaDevices.getUserMedia) {
 			console.log(error);
 		});
 }
+
+takePhotoButton.addEventListener("click", function(event) {
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	// In this section, we set the user authentication, user and app ID, model details, and the URL
+	// of the image we want as an input. Change these strings to run your own example.
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Your PAT (Personal Access Token) can be found in the portal under Authentification
+	const PAT = 'b3f978358e3449239618d3f90b9e098b';
+	// Specify the correct user_id/app_id pairings
+	// Since you're making inferences outside your app's scope
+	const USER_ID = 'clarifai';       
+	const APP_ID = 'main';
+	// Change these to whatever model and image URL you want to use
+	const MODEL_ID = 'food-item-v1-recognition';
+	const MODEL_VERSION_ID = 'dfebc169854e429086aceb8368662641';    
+	const IMAGE_URL = 'https://images.pexels.com/photos/6608618/pexels-photo-6608618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+
+	///////////////////////////////////////////////////////////////////////////////////
+	// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+	///////////////////////////////////////////////////////////////////////////////////
+
+	const raw = JSON.stringify({
+	    "user_app_id": {
+	        "user_id": USER_ID,
+	        "app_id": APP_ID
+	    },
+	    "inputs": [
+	        {
+	            "data": {
+	                "image": {
+	                    "url": IMAGE_URL
+	                }
+	            }
+	        }
+	    ]
+	});
+
+	const requestOptions = {
+	    method: 'POST',
+	    headers: {
+	        'Accept': 'application/json',
+	        'Authorization': 'Key ' + PAT
+	    },
+	    body: raw
+	};
+
+	// NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+	// https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+	// this will default to the latest version_id
+
+	fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+	    .then(response => response.text())
+	    .then(result => console.log(result))
+	    .catch(error => console.log('error', error));
+
+});
